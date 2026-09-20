f28a55cceb39fa2521bb19186185d2aa7b532e2b
devgiahuy <huyngse183274@fpt.edu.vn>
2026-09-20 21:14:50 +0700

    fix(deploy): hident brand

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -53,7 +53,7 @@ export type NavItem = {
 export const NAV_ITEMS: NavItem[] = [
   // { icon: PlusCircle, label: "Thêm Đồ Nhanh", path: "/wardrobe/explore" },
   // { icon: Globe, label: "Cộng Đồng", path: "/community" },
-  { icon: Compass, label: "Khám Phá", path: "/brands" },
+  // { icon: Compass, label: "Khám Phá", path: "/brands" },
   { icon: Shirt, label: "Tủ Quần Áo", path: "/wardrobe" },
   { icon: Sparkles, label: "AI Phối Đồ", path: "/ai-stylist" },
   { icon: Images, label: "Trang Phục", path: "/outfits" },
